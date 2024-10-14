28ec3f23100d94d0024edb00b9ce2a3ee58935e2
Debora Bucci <dbucci@protonmail.com>
2024-10-14 17:37:42 -0300

    finish todo app

--- app/src/main/java/com/example/androidmaster/todoapp/CategoriesViewHolder.kt
+++ app/src/main/java/com/example/androidmaster/todoapp/CategoriesViewHolder.kt
@@ -2,31 +2,59 @@ package com.example.androidmaster.todoapp
 
 import android.view.View
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.SimpleItemAnimator
 import com.example.androidmaster.R
+
 
 class CategoriesViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
     private val tvCategoryName: TextView = view.findViewById(R.id.tvCategoryName)
     private val vDivider: View = view.findViewById(R.id.vDivider)
+    private val cvCategory: CardView = view.findViewById(R.id.cvCategory)
 
     fun render(taskCategory: TaskCategory) {
+
+        tvCategoryName.text = taskCategory.isSelected.toString()
+
+        if (taskCategory.isSelected) {
+            cvCategory.setAlpha(1f)
+        } else {
+            cvCategory.setAlpha(0.3f)
+        }
 
         when (taskCategory) {
             TaskCategory.Business -> {
                 tvCategoryName.text = "Negocios"
-                vDivider.setBackgroundColor(ContextCompat.getColor(vDivider.context, R.color.todo_business_category))
+                vDivider.setBackgroundColor(
+                    ContextCompat.getColor(
+                        vDivider.context,
+                        R.color.todo_business_category
+                    )
+                )
             }
 
             TaskCategory.Other -> {
                 tvCategoryName.text = "Otro"
-                vDivider.setBackgroundColor(ContextCompat.getColor(vDivider.context, R.color.todo_other_category))
+                vDivider.setBackgroundColor(
+                    ContextCompat.getColor(
+                        vDivider.context,
+                        R.color.todo_other_category
+                    )
+                )
             }
 
             TaskCategory.Personal -> {
                 tvCategoryName.text = "Personal"
-                vDivider.setBackgroundColor(ContextCompat.getColor(vDivider.context, R.color.todo_personal_category))
+                vDivider.setBackgroundColor(
+                    ContextCompat.getColor(
+                        vDivider.context,
+                        R.color.todo_personal_category
+                    )
+                )
+
             }
         }
     }
--- app/src/main/java/com/example/androidmaster/todoapp/TodoActivity.kt
+++ app/src/main/java/com/example/androidmaster/todoapp/TodoActivity.kt
@@ -1,25 +1,40 @@
 package com.example.androidmaster.todoapp
 
+import android.app.Dialog
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.androidmaster.R
+import com.example.androidmaster.todoapp.TaskCategory.*
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.navigation.NavigationBarView.OnItemSelectedListener
 
 class TodoActivity : AppCompatActivity() {
 
     private lateinit var rvCategories: RecyclerView
-
     private lateinit var categoriesAdapter: CategoriesAdapter
 
+    private lateinit var rvTasks: RecyclerView
+    private lateinit var taskAdapter: TasksAdapter
+
+    private lateinit var fabAddTask: FloatingActionButton
+
     private val categories = listOf(
-        TaskCategory.Business,
-        TaskCategory.Personal,
-        TaskCategory.Other,
-        )
+        Business,
+        Personal,
+        Other,
+    )
+
+    private val tasks = mutableListOf(
+        Task("Prueba", Business),
+        Task("Prueba", Personal),
+        Task("Prueba", Other),
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,15 +42,79 @@ class TodoActivity : AppCompatActivity() {
 
         initComponent()
         initUI()
+        initListeners()
     }
 
     private fun initComponent() {
         rvCategories = findViewById(R.id.rvCategories)
+        rvCategories.itemAnimator = null
+        rvTasks = findViewById(R.id.rvTasks)
+        fabAddTask = findViewById(R.id.fabAddTask)
     }
 
     private fun initUI() {
-        categoriesAdapter = CategoriesAdapter(categories)
-        rvCategories.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        categoriesAdapter = CategoriesAdapter(categories) {pos -> updateCategories(pos)}
+        rvCategories.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
         rvCategories.adapter = categoriesAdapter
+
+        taskAdapter = TasksAdapter(tasks) { position -> onItemSelected(position) }
+        rvTasks.layoutManager = LinearLayoutManager(this)
+        rvTasks.adapter = taskAdapter
+    }
+
+    private fun initListeners() {
+        fabAddTask.setOnClickListener {
+            showDialog()
+        }
+    }
+
+    private fun showDialog() {
+        val dialog = Dialog(this)
+        dialog.setContentView(R.layout.dialog_task)
+
+        val btnAddTask: Button = dialog.findViewById(R.id.btnAddTask)
+        val etTask: EditText = dialog.findViewById(R.id.etTask)
+        val rgCategories: RadioGroup = dialog.findViewById(R.id.rgCategories)
+
+        btnAddTask.setOnClickListener {
+            val selectedId = rgCategories.checkedRadioButtonId
+            val selectedRB: RadioButton = rgCategories.findViewById(selectedId)
+            val taskText = etTask.text.toString()
+
+            val currentCat: TaskCategory = when (selectedRB.text) {
+                getString(R.string.todo_business) -> Business
+                getString(R.string.todo_personal) -> Personal
+                else -> Other
+            }
+
+            if (taskText.isNotEmpty()) {
+                tasks.add(Task(taskText, currentCat))
+                updateTasks()
+                dialog.hide()
+            }
+        }
+
+        dialog.show()
+    }
+
+    private fun updateTasks() {
+        taskAdapter.notifyDataSetChanged()
+    }
+
+    private fun onItemSelected(position: Int) {
+        tasks[position].isSelected = !tasks[position].isSelected
+        updateTasks()
+    }
+
+    private fun updateCategories(position: Int) {
+        categories[position].isSelected = !categories[position].isSelected
+        categoriesAdapter.notifyItemChanged(position)
+
+        val selectedCategories: List<TaskCategory> = categories.filter {it.isSelected}
+        val selectedTasks = tasks.filter {selectedCategories.contains(it.category)}
+
+       taskAdapter.tasks = selectedTasks
+       updateTasks()
     }
 }
